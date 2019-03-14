5a68fe71e78b51dc688c36e76fe508ccf7f1aa14
tozech <tozech@gmail.com>
2019-03-14 18:42:12 +0100

    install e ensembleMOS

---
+++ run_tutorial.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source activate r-env
+Rscript tutorial.r
---
+++ tutorial.r
@@ -0,0 +1,18 @@
+
+a <- 2
+
+a
+
+library(dplyr)
+iris
+
+iris[2, 'Sepal.Width']
+
+0.98 * 300 + 0.02 * 350
+
+(48 * 35 + 2 * 80) / 50.
+
+library(ggplot2)
+ggplot(data=iris, aes(x=Sepal.Length, y=Sepal.Width, color=Species)) + geom_point(size=3)
+
+
